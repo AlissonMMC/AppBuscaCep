1d36fd84c8fbc12203f7f7fb2179583b5aba261b
Usuario <Usuario@CPD03-M05>
2024-05-16 15:02:54 -0300

    inicio

--- AppBuscaCep/Models/Cep.cs
+++ AppBuscaCep/Models/Cep.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace AppBuscaCep.Models
 {
-    internal class Cep
+    public class Cep
     {
+
+        public int CEP { get; set; }
+        public int id_logradouro { get; set; }
+        public string tipo {  get; set; }
+        public string descricao { get; set; }
+        public int id_cidade { get; set; }
+        public string UF { get; set; }
+        public string complemento { get; set; }
+        public string descricao_sem_numero { get; set; }
+        public string descricao_cidade { get; set; }
+        public int codigo_cidade_ibge { get; set; }
+        public string descricao_bairro { get; set; }
+
+
+
+
+
+
     }
 }
--- AppBuscaCep/Models/Endereco.cs
+++ AppBuscaCep/Models/Endereco.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace AppBuscaCep.Models
 {
-    internal class Endereco
+    public class Endereco
     {
+        public int id_logadouro { get; set; }
+        public int id_cidade { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public string complemento { get; set; }
+        public string descricao_sem_numeros {  get; set; }
+        public string descricao_cidade { get; set; }
+        public string codigo_cidade_ibge { get; set; }
+        public string descricao_bairro { get; set; }
+        public object row { get; set; }
+        public int CEP {  get; set; }
+        public string UF { get; set; }
+
     }
 }
